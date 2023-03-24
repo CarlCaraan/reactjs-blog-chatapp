77980345190860d3d3403b1f32c64801e1a789ec
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-24 21:51:23 +0800

    3/24/23

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import Create from "./pages/create/Create";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
-import Project from "./pages/project/Project";
+import Project from "./pages/project/ProjectDetails";
 
 function App() {
   const { authIsReady, user } = useAuthContext();
