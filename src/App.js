06572938cb10794b4779cf74e05f43681368e68b
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-24 21:49:30 +0800

    3/24/23

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// Styles
 import "./App.css";
 
 // Context Api
 import { useAuthContext } from "./hooks/useAuthContext";
 
+// Pages and Components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
+
 function App() {
   const { authIsReady, user } = useAuthContext();
 
-  return <div className="App">{authIsReady && <div></div>}</div>;
+  return (
+    <div className="App">
+      {authIsReady && (
+        <BrowserRouter>
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Dashboard />
+              </Route>
+              <Route path="/create">
+                <Create />
+              </Route>
+              <Route path="/projects/:id">
+                <Project />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/signup">
+                <Signup />
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      )}
+    </div>
+  );
 }
 
 export default App;
