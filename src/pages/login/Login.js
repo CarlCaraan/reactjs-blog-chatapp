6e722619588c5f0e83584d55a85bd650b172326c
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-27 20:13:48 +0800

    login template

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Styles
 import "./Login.css";
 
+// Custom Hooks
+import { useLogin } from "../../hooks/useLogin";
+
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { error, isPending, login } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(email, password, displayName, thumbnail);
+    login(email, password);
+  };
+
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading...
+        </button>
+      )}
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
 }
 
 export default Login;
