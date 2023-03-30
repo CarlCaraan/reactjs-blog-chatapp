26261fcbf1a5aca623a81e02dc6e0f0ff813ab72
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-30 18:31:57 +0800

    3/30/23

--- src/pages/project/ProjectDetails.js
+++ src/pages/project/ProjectDetails.js
@@ -1,10 +1,54 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+// Custom Hooks
+import { useDocument } from "../../hooks/useDocument";
 
 // Styles
 import "./ProjectDetails.css";
 
 function ProjectDetails() {
-  return <div>ProjectDetails</div>;
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
+
+  // Error Template
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  // Loading Template
+  if (!document) {
+    return (
+      <div class="loader">
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+            <path
+              fill="#8D69F1"
+              d="M158 77c6 23-8 48-28 63-21 16-49 21-68 8-19-12-28-43-20-68s33-45 58-45c26 0 52 20 58 42z"
+            ></path>
+          </svg>
+        </span>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+            <path
+              fill="#8D69F1"
+              d="M158 77c6 23-8 48-28 63-21 16-49 21-68 8-19-12-28-43-20-68s33-45 58-45c26 0 52 20 58 42z"
+            ></path>
+          </svg>
+        </span>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+            <path
+              fill="#8D69F1"
+              d="M158 77c6 23-8 48-28 63-21 16-49 21-68 8-19-12-28-43-20-68s33-45 58-45c26 0 52 20 58 42z"
+            ></path>
+          </svg>
+        </span>
+      </div>
+    );
+  }
+
+  return <div></div>;
 }
 
 export default ProjectDetails;
