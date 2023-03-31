72a8c711637f6014f89879a7c1d2916b078977bb
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-31 14:35:19 +0800

    3/31/23

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -34,6 +34,14 @@ const firestoreReducer = (state, action) => {
         success: true,
         error: null,
       };
+    case "UPDATED_DOCUMENT":
+      return {
+        // ...state,
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
     case "ERROR":
       return {
         // ...state,
@@ -91,10 +99,27 @@ export const useFirestore = (collection) => {
     }
   };
 
+  // Update a document
+  const updateDocument = async (id, updates) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const updatedDocument = await ref.doc(id).update(updates);
+      dispatchIfNotCancelled({
+        type: "UPDATED_DOCUMENT",
+        payload: updateDocument,
+      });
+      return updatedDocument;
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+      return null;
+    }
+  };
+
   // Cleanup Function
   useEffect(() => {
     return () => setIsCancelled(true);
   }, []);
 
-  return { addDocument, deleteDocument, response };
+  return { addDocument, deleteDocument, updateDocument, response };
 };
