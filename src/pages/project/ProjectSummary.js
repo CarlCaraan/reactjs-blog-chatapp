3a4977b1a7e05d6b490d9e915c01a7ecd92b01ac
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-04-01 13:32:17 +0800

    delete project

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 // Styles and components
 import Avatar from "../../components/Avatar";
 
+// Custom Hooks
+import { useFirestore } from "../../hooks/useFirestore";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
 function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore("projects");
+  const { user } = useAuthContext();
+  const history = useHistory();
+
+  const handleClick = (e) => {
+    deleteDocument(project.id);
+    history.push("/");
+  };
+
   return (
     <div>
       <div className="project-summary">
         <h2 className="project-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-date">
           Project due by {project.dueDate.toDate().toDateString()}
         </p>
@@ -21,6 +36,11 @@ function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClick}>
+          Mark as Complete
+        </button>
+      )}
     </div>
   );
 }
